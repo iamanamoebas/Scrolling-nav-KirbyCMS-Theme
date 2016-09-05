a7106a5c865c83fd730e957d94f8dad08b25e4a5
TheSmithy <krullsimon205@gmail.com>
2016-09-05 15:48:33 +0200

    Edit Snippets&Blueprints

--- site/snippets/contact-section.php
+++ site/snippets/contact-section.php
@@ -2,11 +2,11 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1><?php echo $page->title3()->kirbytext() ?></h1>
+                    <h1><?php echo $page->contacttitle()->kirbytext() ?></h1>
                 </div>
             </div
             <div class="text">
-            <p><?php echo $page->text3()->kirbytext() ?></p>
+            <p><?php echo $page->contacttext()->kirbytext() ?></p>
             </div>
         </div>
     </section>
--- site/snippets/services-section.php
+++ site/snippets/services-section.php
@@ -2,11 +2,11 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1><?php echo $page->title2()->kirbytext() ?></h1>
+                    <h1><?php echo $page->servicetitle()->kirbytext() ?></h1>
                 </div>
             </div>
             <div class="text">
-            <p><?php echo $page->text2()->kirbytext() ?></p>
+            <p><?php echo $page->servicetext()->kirbytext() ?></p>
         </div>
         </div>
     </section>
